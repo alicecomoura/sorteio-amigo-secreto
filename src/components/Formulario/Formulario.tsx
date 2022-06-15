6e5e8f23aae5e8dd87a1636115b4abb49951ad2e
Alice Coelho <alicecomoura@outlook.com>
2022-06-15 14:41:34 -0300

    create add participants function and control input

--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,11 +1,32 @@
+import { useRef, useState } from "react"
+
+import { useAdicionarParticipantes } from '../../state/hook/useAdicionarParticipantes'
+
 const Formulario = () => {
+    const adicionarParticipantes = useAdicionarParticipantes()
+
+    const [nome, setNome] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const adicionarParticipamentes = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        adicionarParticipantes(nome)
+        setNome('')
+        inputRef.current?.focus()
+    }
+
     return (
-        <form>
-            <input 
+        <form onSubmit={adicionarParticipamentes}>
+            <input
+                ref={inputRef}
+                type='text'
+                value={nome}
+                onChange={evento => setNome(evento.target.value)} 
                 placeholder='Insira os nomes dos participantes'
             />
 
-            <button disabled>
+            <button disabled={!nome}>
                 Adicionar
             </button>
         </form>
