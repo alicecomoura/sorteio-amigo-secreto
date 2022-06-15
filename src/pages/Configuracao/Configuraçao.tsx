c4ed61c917d33890aaf5fc1855f3b8a1c0709b4d
Alice Coelho <alicecomoura@outlook.com>
2022-06-15 19:11:59 -0300

    structure configuration

---
+++ src/pages/Configuracao/Configuraçao.tsx
@@ -0,0 +1,13 @@
+import Formulario  from '../../components/Formulario/Formulario'
+import ListaDeParticipantes  from '../../components/ListaDeParticipantes/ListaDeParticipantes'
+import Rodape  from '../../components/Rodape/Rodape'
+
+export const Configuracao = () => {
+    return (
+        <>
+            <Formulario />
+            <ListaDeParticipantes />
+            <Rodape />
+        </>
+    )
+}
