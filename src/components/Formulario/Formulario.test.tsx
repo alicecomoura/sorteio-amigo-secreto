e7fdbecda093906c617dfcb44279dfc3db99a8a5
Alice Coelho <alicecomoura@outlook.com>
2022-06-15 14:42:08 -0300

    add test to form

--- src/components/Formulario/Formulario.test.tsx
+++ src/components/Formulario/Formulario.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react' 
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Formulario from './Formulario'
+import { RecoilRoot } from 'recoil'
 
 // Jest
 
@@ -19,4 +20,35 @@ test('quando o input está vazio, novos participantes não podem ser adicionados
     // garantir que o botão esteja desabilitado
     expect(botao).toBeDisabled()
 
+})
+
+test('adiciona um participamente caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>
+    )
+    
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+
+    // inserir um valor no input 
+    fireEvent.change(input, {
+        target: {
+            value: 'Ana Catarina'
+        }
+    })
+
+    // clicar no botão de submeter
+    fireEvent.click(botao)
+
+    // garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+
+    // garantir que o input não tenha valor
+    expect(input).toHaveValue('')
+    
 })
