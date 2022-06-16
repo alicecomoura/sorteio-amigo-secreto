92560f3a08db499d43484b589e5b4d9afc559f3c
Alice Coelho <alicecomoura@outlook.com>
2022-06-16 17:38:49 -0300

    secret friend giveaway display test

--- src/pages/Sorteio/Sorteio.test.tsx
+++ src/pages/Sorteio/Sorteio.test.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { RecoilRoot } from 'recoil'
 import { useListaDeParticipantes } from '../../state/hook/useListaDeParticipantes'
 import Sorteio from './Sorteio'
+import { useResultadoDoSorteio } from '../../state/hook/useResultadoDoSorteio'
 
 jest.mock('../../state/hook/useListaDeParticipantes', () => {
     return {
         useListaDeParticipantes: jest.fn()
+    }
+})
+
+jest.mock('../../state/hook/useResultadoDoSorteio', () => {
+    return {
+        useResultadoDoSorteio: jest.fn()
     }
 })
 
@@ -18,8 +25,15 @@ describe('na pagina do sorteio', () => {
         'Pedro'
     ]
 
+    const resultado = new Map([
+        ['Ana', 'Catarina'],
+        ['Pedro', 'Ana'],
+        ['Catarina', 'Pedro'],
+    ])
+
     beforeEach(() => {
-        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+        (useResultadoDoSorteio as jest.Mock).mockReturnValue(resultado)
     })
 
     test('todos os participantes podem exibir o seu amigo secreto', () => {
@@ -32,5 +46,27 @@ describe('na pagina do sorteio', () => {
         const opcoes = screen.queryAllByRole('option')
 
         expect(opcoes).toHaveLength(participantes.length)
+    })
+
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(
+            <RecoilRoot>
+                <Sorteio />
+            </RecoilRoot>
+        )
+
+        const select = screen.getByPlaceholderText('Selecione o seu nome')
+
+        fireEvent.change(select, {
+            target: {
+                value: participantes[0]
+            }
+        })
+
+        const botao = screen.getByRole('button')
+        fireEvent.click(botao)
+
+        const amigoSecreto = screen.getByRole('alert')
+        expect(amigoSecreto).toBeInTheDocument()
     })
 })
